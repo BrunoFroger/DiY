7d365e7d52b2ab79f4d3e2239c0418a64bf7270a
Bruno FROGER <bruno.froger@orange.com>
2021-03-04 17:27:17 +0100

    debug connexion a un routeur internet

--- regul chauffage avec plusieurs esp/esp32_gateway/esp32_gateway.ino
+++ regul chauffage avec plusieurs esp/esp32_gateway/esp32_gateway.ino
@@ -41,7 +41,7 @@ char buffer [200];
 //const char *routeurSsid     = "Livebox-006d";
 //const char *routeurPwd = "A23F1F7979C9DD3C5916324123";
 const char *routeurSsid = "TP-LINK_88E2";
-const char *routeurPwd  = "74546007";
+const char *routeurPwd  = "75546007";
 
 //=================================================
 //
